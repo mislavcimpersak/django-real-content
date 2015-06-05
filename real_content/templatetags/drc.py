0954d3b4fc77d67f59323c501ca76dc9082d0b67
Mislav Cimperšak <mislav.cimpersak@styria.hr>
2015-06-05 12:53:55 +0100

    title tag refactored

--- real_content/templatetags/drc.py
+++ real_content/templatetags/drc.py
@@ -1,10 +1,26 @@
 # -*- coding: utf-8 -*-
 
+import codecs
+import os
+import random
+
 from django import template
+from django.conf import settings
 
 # from real_content.models import Language, Content
 
 register = template.Library()
+
+
+def random_line(text_file):
+    u"""
+    Gets a random line from a file ignoring lines starting with #.
+    """
+    lines = text_file.read().splitlines()
+    clean_lines = [line for line in lines if line.startswith('#') is False]
+    if clean_lines == []:
+        return 'no usable lines in language file'
+    return random.choice(clean_lines)
 
 
 @register.inclusion_tag('real_content/tags/drc_title.html')
@@ -13,23 +29,26 @@ def drc_title(heading_level=1, css_class='', language=''):
     Retrieve random title.
     Uses langauge from global settings if an override is not provided.
     """
-    return
-    # languages = Language.get_languages(language)
+    if language == '':
+        language = settings.DRC_LANGUAGE
+    language = language.strip()
 
-    # data = {}
-    # content = Content.randoms.filter(
-    #         language__in=languages,
-    #         content_category=Content.CONTENT_CATEGORY.title)\
-    #     .first()
-    # if content:
-    #     data['title'] = content.content
+    content_dir = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), os.pardir, 'content'))
+    content_file = os.path.join(content_dir, '{}_titles.txt'.format(language))
 
-    # if heading_level > 6:
-    #     data['heading_level'] = 6
-    # else:
-    #     data['heading_level'] = heading_level
-    # data['css_class'] = css_class
-    # return data
+    hr_titles = codecs.open(content_file, 'r', 'utf-8')
+
+    data = {
+        'title': random_line(hr_titles),
+    }
+
+    if heading_level > 6:
+        data['heading_level'] = 6
+    else:
+        data['heading_level'] = heading_level
+    data['css_class'] = css_class
+    return data
 
 
 @register.inclusion_tag('real_content/tags/drc_paragraph.html')
