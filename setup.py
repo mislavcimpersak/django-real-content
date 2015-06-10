7c0f631ba381e859173a3b0a7a945c3cc9a13692
Mislav Cimperšak <mislav.cimpersak@styria.hr>
2015-06-10 10:55:12 +0200

    forgot to add content to manifest. sigh

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 
 setup(
     name='django-real-content',
-    version='0.1.0',
+    version='0.1.2',
     packages=find_packages(),
     # packages=find_packages(exclude=("tests",)),
     include_package_data=True,
